7d34b9176af31dada1451fd965bc4ce4c556e28a
radhen <radhen.17@gmail.com>
2019-01-08 16:30:33 -0700

    nn regression working

--- scripts/nn_regress.py
+++ scripts/nn_regress.py
@@ -21,18 +21,41 @@ np.random.seed(0)
 #                                    noise = 0.0,
 #                                    random_state = 0)
 
-df = pd.read_excel('/home/radhen/Documents/expData/motion1/all/motion1_all_15N.xlsx',sheet_name='Sheet2',header=None)
-all_15 = df.as_matrix()
+df_15 = pd.read_excel('/home/radhen/Documents/expData/motion1/all/motion1_all.xlsx',sheet_name='Sheet1',header=None)
+df_20 = pd.read_excel('/home/radhen/Documents/expData/motion1/all/motion1_all.xlsx',sheet_name='Sheet2',header=None)
+df_25 = pd.read_excel('/home/radhen/Documents/expData/motion1/all/motion1_all.xlsx',sheet_name='Sheet3',header=None)
+df_30 = pd.read_excel('/home/radhen/Documents/expData/motion1/all/motion1_all.xlsx',sheet_name='Sheet4',header=None)
+df_35 = pd.read_excel('/home/radhen/Documents/expData/motion1/all/motion1_all.xlsx',sheet_name='Sheet5',header=None)
 
-F = np.sqrt(np.square(all_15[:,5]) + np.square(all_15[:,6]) + np.square(all_15[:,7]))
-theta = all_15[:,6]
-alpha = all_15[:,7]
+all_15 = df_15.as_matrix()
+all_15 = all_15[950:,:]
+all_20 = df_20.as_matrix()
+all_20 = all_20[750:,:]
+all_25 = df_25.as_matrix()
+all_25 = all_25[150:,:]
+all_30 = df_30.as_matrix()
+all_30 = all_30[350:,:]
+all_35 = df_35.as_matrix()
+all_35 = all_35[500:,:]
 
-# all_15 = np.column_stack((all_15[:,16], F))
+all = all_15
+all = np.append(all,all_20,axis=0)
+all = np.append(all,all_25,axis=0)
+all = np.append(all,all_30,axis=0)
+all = np.append(all,all_35,axis=0)
+
+
+
+F = np.sqrt(np.square(all[:,8]) + np.square(all[:,9]) + np.square(all[:,10])) # F = sqrt(Fx^2+Fy^2+Fz^2)
+theta = all[:,6]
+alpha = all[:,7]
+
 target = np.column_stack((F, theta))
-target = np.column_stack((target, alpha))
+target = np.column_stack((target, alpha)) # [F, theta, alpha]
+features = all[:,13:15] # [baro, ir]
 
-features = all_15[:,13:15]
+# plt.plot(target[:,0], 'b', ms=1.5, label='Measurement')
+# plt.show()
 
 # Divide our data into training and test sets
 train_features, test_features, train_target, test_target = train_test_split(features,
